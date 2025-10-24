6fe8b0411310bfb0ff2be9e9b6df78f7630c4557
SadafImaduddin <imaduddinsadaf@hotmail.com>
2025-10-23 23:54:09 -0700

    second commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const searchInput = document.querySelector('.search');
 const movie = document.querySelectorAll('.movie');
 
 searchInput.addEventListener('keyup', function(event){
-    const word = event.target.value;
+    const word = event.target.value.toLowerCase();
 
     movie.forEach(function(item){
         item.querySelector('p').textContent.toLowerCase().includes(word) ? (item.style.display = 'block') : (item.style.display = 'none');
